380e11e8e71413ea2a8fa147a4d89ea1b79161e3
couzhei <couzhei@gmail.com>
2025-02-10 09:57:25 +0330

    - GH commits

---
+++ ui/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/couzhei.github.io",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
--- ui/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Enables static export
-  basePath: '/couzhei.github.io', // Add basePath for GitHub Pages
-  assetPrefix: '/couzhei.github.io/', // Add assetPrefix for GitHub Pages
-};
-
-module.exports = nextConfig;
