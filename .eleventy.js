cc205e68538c3eb331fbf75acac6d00da37360c9
Vincent Law <crudyn@localhost>
2026-02-22 02:32:04 +0330

    Remove unused static assets and CSS files, including webpack chunks, font files, and build manifests. Add .eleventyignore to exclude specific directories from processing.

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,15 @@
 module.exports = function (eleventyConfig) {
+  // Pass through copy for static assets
   eleventyConfig.addPassthroughCopy({ "src/styles.css": "styles.css" });
   eleventyConfig.addPassthroughCopy({ "src/resume.pdf": "resume.pdf" });
-  eleventyConfig.addPassthroughCopy({ "src/projects": "projects" });
-  eleventyConfig.addPassthroughCopy({ "src/blog": "blog" });
+
   return {
     dir: {
       input: "src",
       output: "_output",
+      includes: "layouts",
+      layouts: "layouts",
     },
+    pathPrefix: "/",
   };
 };
