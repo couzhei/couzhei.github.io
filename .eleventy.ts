3d1ce21426900db0aefa669c3258d41f316a8790
Vincent Law <crudyn@localhost>
2026-02-22 13:16:27 +0330

    🔧 Add custom postDate filter for consistent date formatting

--- .eleventy.ts
+++ .eleventy.ts
@@ -3,6 +3,13 @@ import type { EleventyConfig } from "@11ty/eleventy";
 const config = function (eleventyConfig: EleventyConfig) {
   // Pass through copy for public assets
   eleventyConfig.addPassthroughCopy({ public: "." });
+  eleventyConfig.addFilter("postDate", (dateObj: Date) => {
+    return new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    }).format(dateObj);
+  });
 
   return {
     dir: {
